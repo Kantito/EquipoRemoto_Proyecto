e4970b8ab095c26db53fe73d449e5d05729e3f82
malpz <mauricio.alpizar.alpizar@est.una.ac.cr>
2023-12-02 14:29:08 -0600

    .

--- EquipoRemoto_Proyecto/Program.cs
+++ EquipoRemoto_Proyecto/Program.cs
@@ -8,6 +8,8 @@ namespace EquipoRemoto_Proyecto
         [STAThread]
         static void Main()
         {
+            
+            //.
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
